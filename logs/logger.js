f89083b3d869785b85d56239f346ed99a2fb2d2c
Pablo Arauz Caballero <128090351+PabloArauzCaballero@users.noreply.github.com>
2026-05-03 23:00:36 -0400

    logger

--- logs/logger.js
+++ logs/logger.js
@@ -1,15 +1,21 @@
 const pino = require("pino");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const logger = pino({
     name: "cpa-plataforma",
     level: process.env.LOG_LEVEL || "info",
-    transport: {
-        target: "pino/file",
-        options:{
-            destination: "./logs/app.log",
-            mkdir: true,
-        },
-    },
+    ...(isProduction
+        ? {}
+        : {
+              transport: {
+                  target: "pino/file",
+                  options: {
+                      destination: "./logs/app.log",
+                      mkdir: true,
+                  },
+              },
+          }),
 });
 
 module.exports = logger;
